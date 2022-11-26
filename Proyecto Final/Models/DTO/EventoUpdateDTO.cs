906d69e9e88ecfaecfae25f88ad375cb6e26b54e
Jesus Ruiz <alex.malen@gmail.com>
2022-11-26 01:36:26 -0600

    Subidos los DTO faltantes y edit funsional

---
+++ Proyecto Final/Models/DTO/EventoUpdateDTO.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Proyecto_Final.Models.DTO
+{
+    public class EventoUpdateDTO
+    {
+        [Key]
+        [Column("idEvento")]
+        public int IdEvento { get; set; }
+        [Column("idUsuario")]
+        public int IdUsuario { get; set; }
+        [Column("tituloEvento")]
+        [StringLength(50)]
+        public string TituloEvento { get; set; } = null!;
+        [Column("contenidoEvento")]
+        public string ContenidoEvento { get; set; } = null!;
+        [Column("estatusEvento")]
+        public bool EstatusEvento { get; set; }
+        [Column("fechaEvento", TypeName = "datetime")]
+        public DateTime FechaEvento { get; set; }
+        [Column("foto")]
+        public string? Foto { get; set; }
+
+    }
+}
--- Proyecto Final/Models/DTO/NoticiaUpdateDTO.cs
+++ Proyecto Final/Models/DTO/NoticiaUpdateDTO.cs
@@ -4,21 +4,17 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Proyecto_Final.Models.DTO
 {
-    public class NoticiaCreateDTO
+    public class NoticiaUpdateDTO
     {
-        [Column("idUsuario")]
-        public int IdUsuario { get; set; }
-        [Column("tituloEvento")]
-        [StringLength(50)]
         public string tituloNoticia { get; set; } = null!;
         [Column("contenidoNoticia")]
         public string contenidoNoticia { get; set; } = null!;
-        [Column("estatusNoticia")]
-        public bool noticiaStatus { get; set; }
         [Column("fechaNoticia", TypeName = "datetime")]
         public DateTime fechaNoticia { get; set; }
         [Column("foto")]
         public string? Foto { get; set; }
         public IFormFile? FotoFile { get; set; }
+        [Column("estatusNoticia")]
+        public bool noticiaStatus { get; set; }
     }
 }
